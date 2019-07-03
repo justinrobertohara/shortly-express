5d6e6a26574a9a3b3a59d613ea0669c77775696a
Justin O'Hara <justinrobertohara@gmail.com>
2019-07-02 21:33:42 -0700

    Initialized express, bodyParser and cookieParser

--- app/models/user.js
+++ app/models/user.js
@@ -1,10 +1,22 @@
 var db = require('../config');
 var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
+var bodyParser = require('bodyParser');
+var express = require('express');
+
+var app = express();
+app.use(express.bodyParser());
+app.use(express.cookieParser('here is our string'))
+
+// console.log('this is our app in user.js******', app);
 
 
+//Allow users to register for a new account, or to login
+//Build pages for login and sign up
+//And add routes to process the form data using POST actions.
 
 var User = db.Model.extend({
+
 });
 
 module.exports = User;
